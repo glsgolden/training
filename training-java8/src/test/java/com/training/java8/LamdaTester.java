36ce81498145878680dd81f4d55808cbe50b7ee1
Ganesh Shirsat <glsshirsat.java@gmail.com>
2015-04-01 08:17:58 +0530

    Update Test Classes

--- training-java8/src/test/java/com/training/java8/LamdaTester.java
+++ training-java8/src/test/java/com/training/java8/LamdaTester.java
@@ -26,8 +26,16 @@ public class LamdaTester
 	@Test
 	public void withReturnAndCurlyBraces()
 	{
-		LambdaExpression.MathOperation substraction = (a, b) -> {return (a - b) };
-		System.out.println("Addition of two number =" + lamdaExpression.operate(10, 20, substraction));
+		LambdaExpression.MathOperation multiplication = (a, b) -> {return (a * b); };
+		System.out.println("Multiplication of two number =" + lamdaExpression.operate(10, 20, multiplication));
+		
+	}
+	
+	@Test
+	public void withReturnAndNoCurlyBraces()
+	{
+		LambdaExpression.MathOperation division = (a, b) -> (b / a) ;
+		System.out.println("Division of two number =" + lamdaExpression.operate(10, 20, division));
 		
 	}
 
